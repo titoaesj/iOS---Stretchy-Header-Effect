69c112a1cf944ae069bb6166c3b5c5053b1e151b
Tito Albino Evangelista da Silva Junior <titoaesj@gmail.com>
2018-12-26 15:49:07 -0300

    Initial Commit

---
+++ StretchyHeaderEffect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StretchyHeaderEffect
+//
+//  Created by Tito Albino Evangelista da Silva Junior on 26/12/18.
+//  Copyright © 2018 br.com.titoaesj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
